e86c5c030bbde2c32afe7459d67268c2b7aa03a1
FedericoSavastano <f.savastano@hotmail.com>
2022-03-12 17:18:04 -0300

    Correccion desafio 07

--- Desafio 07/src/NPMExpress_SavastanoFederico.js
+++ Desafio 07/src/NPMExpress_SavastanoFederico.js
@@ -1,3 +1,40 @@
+// import fs from 'fs';
+// import express from 'express';
+
+// const app = express();
+// const PORT = 8080;
+// const server = app.listen(PORT, () => {
+//     console.log(`Listening on port ${PORT}`);
+// });
+
+// let viewsProducts = 0;
+// let viewsOneProduct = 0;
+// const fileRoute = './files/productos.txt';
+
+// app.get('/items', (req, res) => {
+//     let content = fs.readFileSync(fileRoute, 'utf-8');
+
+//     res.send(`Items: ${JSON.stringify(JSON.parse(content))},
+//     cantidad: ${JSON.parse(content).length}`);
+
+//     viewsProducts++;
+// });
+
+// app.get('/item-random', (req, res) => {
+//     let content = fs.readFileSync(fileRoute, 'utf-8');
+//     let parsed = JSON.parse(content);
+//     const randomNum = (min, max) => Math.round(Math.random() * max + min);
+//     let randomNumber = randomNum(0, parsed.length - 1);
+
+//     res.send(`Item: ${JSON.stringify(parsed[randomNumber])}`);
+
+//     viewsOneProduct++;
+// });
+
+// app.get('/visitas', (req, res) => {
+//     res.send(`Visitas: Items: ${viewsProducts} Item: ${viewsOneProduct}`);
+// });
+
 import fs from 'fs';
 import express from 'express';
 
@@ -12,12 +49,13 @@ let viewsOneProduct = 0;
 const fileRoute = './files/productos.txt';
 
 app.get('/items', (req, res) => {
-    let content = fs.readFileSync(fileRoute, 'utf-8');
-
-    res.send(`Items: ${JSON.stringify(JSON.parse(content))}, 
-    cantidad: ${JSON.parse(content).length}`);
-
+    let content = JSON.parse(fs.readFileSync(fileRoute, 'utf-8'));
     viewsProducts++;
+
+    res.send({
+        items: content,
+        cantidad: content.length,
+    });
 });
 
 app.get('/item-random', (req, res) => {
@@ -25,12 +63,11 @@ app.get('/item-random', (req, res) => {
     let parsed = JSON.parse(content);
     const randomNum = (min, max) => Math.round(Math.random() * max + min);
     let randomNumber = randomNum(0, parsed.length - 1);
-
-    res.send(`Item: ${JSON.stringify(parsed[randomNumber])}`);
-
     viewsOneProduct++;
+
+    res.send({ Item: parsed[randomNumber] });
 });
 
 app.get('/visitas', (req, res) => {
-    res.send(`Visitas: Items: ${viewsProducts} Item: ${viewsOneProduct}`);
+    res.send({ Visitas: viewsProducts, Item: viewsOneProduct });
 });
